007a17756ebb688fda697648213a0797563e6c88
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2023-11-25 17:06:39 +0100

    added singletion wrapper class

---
+++ src/Hector/Singleton.cs
@@ -0,0 +1,18 @@
+﻿namespace Hector.Core
+{
+    // credits: C# in Depth by Jon Skeet - http://csharpindepth.com/Articles/General/Singleton.aspx
+    public sealed class Singleton<T> where T : class, new()
+    {
+        public static T Instance { get; } = new T();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static Singleton()
+        {
+        }
+
+        private Singleton()
+        {
+        }
+    }
+}
