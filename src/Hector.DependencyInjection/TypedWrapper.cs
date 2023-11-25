279d491ddf5d28a21963e4a40dabff0a1c5f3de0
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2023-11-25 17:14:17 +0100

    added wrapper class for types in DI

---
+++ src/Hector.DependencyInjection/TypedWrapper.cs
@@ -0,0 +1,33 @@
+﻿namespace Hector.Core.DependencyInjection
+{
+    public class TypedWrapper<T>
+    {
+        public T Value { get; }
+
+        public TypedWrapper(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public class TypedWrapper<T1, T2> : TypedWrapper<T1>
+    {
+        public TypedWrapper(T1 value) : base(value)
+        {
+        }
+    }
+
+    public class TypedWrapper<T1, T2, T3> : TypedWrapper<T1>
+    {
+        public TypedWrapper(T1 value) : base(value)
+        {
+        }
+    }
+
+    public class TypedWrapper<T1, T2, T3, T4> : TypedWrapper<T1>
+    {
+        public TypedWrapper(T1 value) : base(value)
+        {
+        }
+    }
+}
