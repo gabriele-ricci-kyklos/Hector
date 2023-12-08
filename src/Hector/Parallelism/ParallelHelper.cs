ec1db6a3500b46bd25605eaed4d5a184ff93228e
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2023-12-08 18:17:04 +0100

    added parallel helper methods

---
+++ src/Hector/Parallelism/ParallelHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hector.Core.Parallelism
+{
+    public class ParallelHelper
+    {
+        public static Task<Dictionary<TKey, TResult[]>> DoInParallelColletingResultsAsync<TKey, TResult>(TKey[] models, Func<TKey, CancellationToken, Task<TResult[]>> actionTask, int degreeOfParallelism = 5)
+            where TKey : notnull
+        {
+            Func<TKey, ConcurrentDictionary<TKey, TResult[]>, CancellationToken, ValueTask> action =
+                async (x, y, z) =>
+                {
+                    var res = await actionTask(x, z).ConfigureAwait(false);
+                    y.TryAdd(x, res);
+                };
+
+            return DoInParallelColletingResultsAsync(models, action, degreeOfParallelism);
+        }
+
+        public static async Task<Dictionary<TKey, TResult[]>> DoInParallelColletingResultsAsync<TKey, TResult>(TKey[] models, Func<TKey, ConcurrentDictionary<TKey, TResult[]>, CancellationToken, ValueTask> actionTask, int degreeOfParallelism = 5)
+            where TKey : notnull
+        {
+            ConcurrentDictionary<TKey, TResult[]> resultDataDict = new(degreeOfParallelism, models.Length);
+
+            await Parallel
+                .ForEachAsync
+                (
+                    models,
+                    new ParallelOptions { MaxDegreeOfParallelism = degreeOfParallelism },
+                    (x, c) => actionTask(x, resultDataDict, c)
+                )
+                .ConfigureAwait(false);
+
+            return resultDataDict.ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
