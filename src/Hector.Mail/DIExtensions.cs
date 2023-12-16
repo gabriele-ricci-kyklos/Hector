c67d73d1257783b9ee585d0c5a16da22d631dbce
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2023-12-16 17:13:52 +0100

    added mail core library

---
+++ src/Hector.Mail/DIExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Hector.Core.Mail
+{
+    public static class DIExtensions
+    {
+        public static IServiceCollection AddMailClient(this IServiceCollection services) =>
+            services
+                .AddSingleton<IMailClient, MailKitMailClient>();
+    }
+}
---
+++ src/Hector.Mail/IMailClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Hector.Core.Mail
+{
+    public interface IMailClient
+    {
+        Task SendMailAsync(MailModel mailModel);
+        Task SendMailAsync(string subject, string body, string[] toAddressList);
+        Task SendMailAsync(string subject, string body, string sender, string[] toAddressList);
+        Task SendMailAsync(bool containsHtml, string subject, string body, string? footer, string sender, string[] toAddressList);
+    }
+}
