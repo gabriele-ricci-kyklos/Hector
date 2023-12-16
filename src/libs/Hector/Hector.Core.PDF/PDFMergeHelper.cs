2f83275dabf7122f94d9f7721bc92c29e738e6a1
Gabriele Ricci <gabriele.ricci@remira.com>
2023-12-16 16:16:31 +0100

    added PDF merge helper class and new PDF library

---
+++ src/libs/Hector/Hector.Core.PDF/PDFMergeHelper.cs
@@ -0,0 +1,100 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace Hector.Core.PDF
+{
+    public static class PDFMergeHelper
+    {
+        public static PdfDocument MergePDFFiles(Stream[] streamList)
+        {
+            using PdfDocument outputDocument = new();
+            outputDocument.PageLayout = PdfPageLayout.TwoColumnLeft;
+            List<PdfPage> pageList = [];
+
+            foreach (Stream stream in streamList)
+            {
+                using PdfDocument inputDocument = PdfReader.Open(stream, PdfDocumentOpenMode.Import);
+                foreach (PdfPage page in inputDocument.Pages)
+                {
+                    outputDocument.AddPage(page);
+                }
+            }
+
+            return outputDocument;
+        }
+
+        public static PdfDocument MergePDFFiles(string[] filePathList)
+        {
+            FileStream[] streams = [];
+
+            try
+            {
+                streams =
+                    filePathList
+                        .Select(x => new FileStream(x, FileMode.Open, FileAccess.Read))
+                        .ToArray();
+
+                return MergePDFFiles(streams);
+            }
+            finally
+            {
+                Array.ForEach(streams, x => x.Dispose());
+            }
+        }
+
+        public static PdfDocument MergePDFFiles(byte[][] fileContentList)
+        {
+            MemoryStream[] streams = [];
+
+            try
+            {
+                streams =
+                    fileContentList
+                        .Select(x => new MemoryStream(x))
+                        .ToArray();
+
+                return MergePDFFiles(streams);
+            }
+            finally
+            {
+                Array.ForEach(streams, x => x.Dispose());
+            }
+        }
+
+        public static void MergePDFFiles(Stream[] streamList, string destFilePath)
+        {
+            using PdfDocument doc = MergePDFFiles(streamList);
+            doc.Save(destFilePath);
+        }
+
+        public static void MergePDFFiles(string[] filePathList, string destFilePath)
+        {
+            using PdfDocument doc = MergePDFFiles(filePathList);
+            doc.Save(destFilePath);
+        }
+
+        public static void MergePDFFiles(byte[][] fileContentList, string destFilePath)
+        {
+            using PdfDocument doc = MergePDFFiles(fileContentList);
+            doc.Save(destFilePath);
+        }
+
+        public static void MergePDFFiles(Stream[] streamList, Stream destStream)
+        {
+            using PdfDocument doc = MergePDFFiles(streamList);
+            doc.Save(destStream);
+        }
+
+        public static void MergePDFFiles(string[] filePathList, Stream destStream)
+        {
+            using PdfDocument doc = MergePDFFiles(filePathList);
+            doc.Save(destStream);
+        }
+
+        public static void MergePDFFiles(byte[][] fileContentList, Stream destStream)
+        {
+            using PdfDocument doc = MergePDFFiles(fileContentList);
+            doc.Save(destStream);
+        }
+    }
+}
