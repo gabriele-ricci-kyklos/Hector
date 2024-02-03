71900fb3903d38d89e1973dbde2085fdd317924e
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2024-02-03 15:51:50 +0100

    added method to get the default value for any type

---
+++ src/Hector.Reflection/TypeExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace Hector.Core.Reflection
+{
+    public static class TypeExtensionMethods
+    {
+        public static object? GetDefaultValue(this Type t) =>
+            typeof(TypeExtensionMethods).GetMethod(nameof(GetDefaultGeneric), BindingFlags.Static | BindingFlags.NonPublic)?.MakeGenericMethod(t)?.Invoke(null, null);
+
+        private static T? GetDefaultGeneric<T>() => default;
+    }
+}
