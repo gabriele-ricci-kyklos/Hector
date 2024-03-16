d4959a6308da382ff7b47e2cba5aca59552595ef
Gabriele Ricci <gabriele.ricci@remira.com>
2024-03-16 18:56:56 +0100

    added dictionary extension methods tests

--- src/libs/Hector/Hector.Core/ExtensionMethods/DictionaryExtensionMethods.cs
+++ src/libs/Hector/Hector.Core/ExtensionMethods/DictionaryExtensionMethods.cs
@@ -5,12 +5,6 @@ namespace Hector.Core
 {
     public static class DictionaryExtensionMethods
     {
-        public static Dictionary<TKey, TResult> ToDictionary<TKey, TResult>(this IEnumerable<KeyValuePair<TKey, TResult>> itemList)
-            where TKey : notnull =>
-            itemList
-                .ToEmptyIfNull()
-                .ToDictionary(x => x.Key, x => x.Value);
-
         public static TValue[] GetValues<TKey, TValue>(this IDictionary<TKey, TValue> dict, IEnumerable<TKey> keyList) =>
             keyList
                 .ToEmptyIfNull()
---
+++ src/libs/Hector/Hector.Tests/Core/ExtensionMethods/DictionaryExtensionMethodsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Hector.Core;
+
+namespace Hector.Tests.Core.ExtensionMethods
+{
+    public class DictionaryExtensionMethodsTests
+    {
+        [Fact]
+        public void TestGetValues()
+        {
+            Dictionary<int, int> dict = new()
+            {
+                {1, 1},
+                {2, 2}
+            };
+
+            dict
+                .GetValues([1, 2])
+                .Should().Equal(1, 2);
+        }
+    }
+}
