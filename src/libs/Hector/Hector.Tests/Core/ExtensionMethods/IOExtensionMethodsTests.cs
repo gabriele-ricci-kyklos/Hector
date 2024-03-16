98188dc0d8b59d984eb2a5ac4f576c12f68e6e6b
Gabriele Ricci <gabriele.ricci@remira.com>
2024-03-16 18:48:06 +0100

    added IO extension methods tests

---
+++ src/libs/Hector/Hector.Tests/Core/ExtensionMethods/IOExtensionMethodsTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Hector.Core;
+
+namespace Hector.Tests.Core.ExtensionMethods
+{
+    public class IOExtensionMethodsTests
+    {
+        [Theory]
+        [InlineData("my", "folder")]
+
+        public void TestCombinePaths(string path1, string path2)
+        {
+            "C:".CombinePaths(path1, path2).Should().Be(@"C:\my\folder");
+        }
+
+        [Fact]
+        public void TestIsUncPath()
+        {
+            @"\\MYSERVER\my\folder".IsUncPath().Should().BeTrue();
+            @"C:\my\folder".IsUncPath().Should().BeFalse();
+        }
+
+        [Fact]
+        public void TestIsAbsolutePath()
+        {
+            @"C:\my\folder".IsAbsolutePath().Should().BeTrue();
+            @"..\my\folder".IsAbsolutePath().Should().BeFalse();
+        }
+    }
+}
