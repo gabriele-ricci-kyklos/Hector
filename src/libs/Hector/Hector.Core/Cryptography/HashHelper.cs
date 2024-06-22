de0f5096906af449d652e76a64bff80232283e82
Gabriele Ricci <gabriele.ricci@remira.com>
2024-06-22 15:40:01 +0200

    improved and tested hash helper class

--- src/libs/Hector/Hector.Core/Cryptography/HashHelper.cs
+++ src/libs/Hector/Hector.Core/Cryptography/HashHelper.cs
@@ -9,24 +9,30 @@ namespace Hector.Core.Cryptography
         public static string ComputeGenericHash(Func<HashAlgorithm> hashProvider, string sourceForHash, Encoding? encoding = null)
         {
             byte[] bytes = (encoding ?? Encoding.UTF8).GetBytes(sourceForHash);
-            using HashAlgorithm hashAlgorithm = hashProvider();
-            byte[] array = hashAlgorithm.ComputeHash(bytes);
-            return Convert.ToBase64String(array);
+            return ComputeGenericHash(hashProvider, bytes);
         }
 
         public static string ComputeGenericHash(Func<HashAlgorithm> hashProvider, byte[] bContent)
         {
             using HashAlgorithm hashAlgorithm = hashProvider();
             byte[] array = hashAlgorithm.ComputeHash(bContent);
-            return Convert.ToBase64String(array);
+            return array.ToHexString();
         }
-
-        public static string ComputeSHA512(string sourceForHash) => ComputeGenericHash(new Func<HashAlgorithm>(SHA512.Create), sourceForHash);
-
-        public static string ComputeSHA512(byte[] bContent) => ComputeGenericHash(new Func<HashAlgorithm>(SHA512.Create), bContent);
 
         public static string ComputeMD5(string sourceForHash) => ComputeGenericHash(new Func<HashAlgorithm>(MD5.Create), sourceForHash);
 
         public static string ComputeMD5(byte[] bContent) => ComputeGenericHash(new Func<HashAlgorithm>(MD5.Create), bContent);
+
+        public static string ComputeSHA1(string sourceForHash) => ComputeGenericHash(new Func<HashAlgorithm>(SHA1.Create), sourceForHash);
+
+        public static string ComputeSHA1(byte[] bContent) => ComputeGenericHash(new Func<HashAlgorithm>(SHA1.Create), bContent);
+
+        public static string ComputeSHA256(string sourceForHash) => ComputeGenericHash(new Func<HashAlgorithm>(SHA256.Create), sourceForHash);
+
+        public static string ComputeSHA256(byte[] bContent) => ComputeGenericHash(new Func<HashAlgorithm>(SHA256.Create), bContent);
+
+        public static string ComputeSHA512(string sourceForHash) => ComputeGenericHash(new Func<HashAlgorithm>(SHA512.Create), sourceForHash);
+
+        public static string ComputeSHA512(byte[] bContent) => ComputeGenericHash(new Func<HashAlgorithm>(SHA512.Create), bContent);
     }
 }
---
+++ src/libs/Hector/Hector.Tests/Core/Cryptography/CryptographyTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Hector.Core.Cryptography;
+using System.Text;
+
+namespace Hector.Tests.Core.Cryptography
+{
+    public class CryptographyTests
+    {
+        private const string _input = "hector";
+        private byte[] _inputBytes = Encoding.UTF8.GetBytes(_input);
+
+        [Fact]
+        public void TestHashHelperMD5()
+        {
+            const string expectedOutput = "3ab9071536d62f29aa8b3fd39141f6ad";
+
+            string hash = HashHelper.ComputeMD5(_input);
+            hash.Should().Be(expectedOutput);
+
+            string bHash = HashHelper.ComputeMD5(_inputBytes);
+            bHash.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        public void TestHashHelperSHA1()
+        {
+            const string expectedOutput = "9c6eb1131b4202ad199765465391a99f6498009c";
+
+            string hash = HashHelper.ComputeSHA1(_input);
+            hash.Should().Be(expectedOutput);
+
+            string bHash = HashHelper.ComputeSHA1(_inputBytes);
+            bHash.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        public void TestHashHelperSHA256()
+        {
+            const string expectedOutput = "51d3ba50d3e136bc03ca019303427831f4f49d88b775b4a529685533c8ce0e65";
+
+            string hash = HashHelper.ComputeSHA256(_input);
+            hash.Should().Be(expectedOutput);
+
+            string bHash = HashHelper.ComputeSHA256(_inputBytes);
+            bHash.Should().Be(expectedOutput);
+        }
+
+        [Fact]
+        public void TestHashHelperSHA5121()
+        {
+            const string expectedOutput = "150fabdef322b295706ecfa27acbfd36496ed754bd27342323d42711da174075575d2a53ef538e37945c19edb3968bb399ce4a6d9cdd80e841d650ed81551c08";
+
+            string hash = HashHelper.ComputeSHA512(_input);
+            hash.Should().Be(expectedOutput);
+
+            string bHash = HashHelper.ComputeSHA512(_inputBytes);
+            bHash.Should().Be(expectedOutput);
+        }
+    }
+}
