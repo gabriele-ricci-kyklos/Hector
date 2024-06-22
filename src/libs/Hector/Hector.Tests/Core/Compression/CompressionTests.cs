9683ad6ea8cd31c46e0b3e10c3794fee91d87275
Gabriele Ricci <gabriele.ricci@remira.com>
2024-06-22 15:48:55 +0200

    added compression tests

---
+++ src/libs/Hector/Hector.Tests/Core/Compression/CompressionTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Hector.Core.Compression;
+
+namespace Hector.Tests.Core.Compression
+{
+    public class CompressionTests
+    {
+        [Theory]
+        [InlineData("hector")]
+        [InlineData("c# programming language")]
+        public async Task TestGZipHelper(string input)
+        {
+            byte[] compressed = await GZipHelper.CompressStringAsync(input);
+            string decompressed = await GZipHelper.DecompressStringAsync(compressed);
+
+            decompressed.Should().Be(input);
+        }
+
+        [Theory]
+        [InlineData("hector")]
+        [InlineData("c# programming language")]
+        public void TestLZ77Helper(string input)
+        {
+            string compressed = LZ77Helper.CompressString(input);
+            string decompressed = LZ77Helper.DecompressStrings(compressed);
+
+            decompressed.Should().Be(input);
+        }
+
+        [Theory]
+        [InlineData("hector")]
+        [InlineData("c# programming language")]
+        public void TestLZWHelper(string input)
+        {
+            byte[] compressed = LZWHelper.Compress(input);
+            string decompressed = LZWHelper.Decompress(compressed);
+
+            decompressed.Should().Be(input);
+        }
+    }
+}
