60e3303ae4153f003c002caeb51ceef5b4b04ea2
Gabriele Ricci <gabriele.ricci@remira.com>
2024-06-23 17:33:44 +0200

    added support for entity property attribute in async dao

--- src/libs/Hector/Hector.Data/DataMapping/EntityDataRecordMapper.cs
+++ src/libs/Hector/Hector.Data/DataMapping/EntityDataRecordMapper.cs
@@ -1,17 +1,22 @@
 ﻿using FastMember;
+using Hector.Core;
 using Hector.Core.Reflection;
+using Hector.Data.Entities.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Hector.Data.DataMapping
 {
+    record EntityPropertyInfo(string PropertyName, string ColumnName);
+
     internal class EntityDataRecordMapper : BaseDataRecordMapper
     {
         private readonly TypeAccessor _typeAccessor;
-        private readonly HashSet<string> _propertiesSet;
+        private readonly Dictionary<string, string> _propertiesMapping;
 
-        public override int FieldsCount => _propertiesSet.Count;
+        public override int FieldsCount => _propertiesMapping.Count;
 
         public EntityDataRecordMapper(Type type, string[] dataRecordColumns)
             : base(type, dataRecordColumns)
@@ -20,11 +25,9 @@ namespace Hector.Data.DataMapping
                 TypeAccessor
                     .Create(_type, true);
 
-            _propertiesSet =
-                _typeAccessor
-                    .GetUnorderedPropertyList()
-                    .Select(x => x.Name)
-                    .ToHashSet(StringComparer.OrdinalIgnoreCase);
+            _propertiesMapping =
+                GetEntityPropertyInfoList()
+                .ToDictionary(x => x.ColumnName, x => x.PropertyName);
         }
 
         public override object Build(int position, DataRecord[] records)
@@ -35,7 +38,7 @@ namespace Hector.Data.DataMapping
 
             for (int i = 0; i < records.Length; ++i)
             {
-                if (!_propertiesSet.TryGetValue(records[i].Name, out string? propertyName))
+                if (!_propertiesMapping.TryGetValue(records[i].Name, out string? propertyName))
                 {
                     continue;
                 }
@@ -44,6 +47,28 @@ namespace Hector.Data.DataMapping
             }
 
             return resultObj;
+        }
+
+        private EntityPropertyInfo[] GetEntityPropertyInfoList()
+        {
+            PropertyInfo[] properties = _type.GetPropertiesForType();
+            EntityPropertyInfo[] results = new EntityPropertyInfo[properties.Length];
+
+            for (int i = 0; i < properties.Length; ++i)
+            {
+                string columnName = properties[i].Name;
+
+                EntityPropertyInfoAttribute? attrib = properties[i].GetAttributeOfType<EntityPropertyInfoAttribute>(true);
+
+                if (attrib is not null)
+                {
+                    columnName = attrib.ColumnName;
+                }
+
+                results[i] = new EntityPropertyInfo(properties[i].Name, columnName);
+            }
+
+            return results.ToArray();
         }
     }
 }
