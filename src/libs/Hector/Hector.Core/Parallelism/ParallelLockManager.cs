e11d9ac4157a3897f6fdbd242b8ba45c4bfb1533
Gabriele Ricci <gabriele.ricci@remira.com>
2024-07-11 23:05:59 +0200

    added parallel lock maganer class

---
+++ src/libs/Hector/Hector.Core/Parallelism/ParallelLockManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hector.Core.Parallelism
+{
+    public class ParallelLockManager<TKey>(int initialCount = 1, int maxCount = 1) : IDisposable
+        where TKey : notnull
+    {
+        private readonly ConcurrentDictionary<TKey, SemaphoreSlim> _keyLocks = new();
+
+        public async Task<T> ExecuteLockedCallAsync<T>(TKey key, Func<Task<T>> action, TimeSpan? timeout = null, CancellationToken ctoken = default)
+        {
+            SemaphoreSlim lockItem = _keyLocks.GetOrAdd(key, x => new SemaphoreSlim(initialCount, maxCount));
+
+            try
+            {
+                await lockItem.WaitAsync(timeout ?? TimeSpan.FromSeconds(30), ctoken).ConfigureAwait(false);
+                return await action().ConfigureAwait(false);
+            }
+            finally
+            {
+                lockItem.Release();
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (SemaphoreSlim item in _keyLocks.Values)
+            {
+                item.Dispose();
+            }
+        }
+    }
+}
---
+++ src/libs/Hector/Hector.Tests/Core/Parallelism/ParallelismTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Hector.Core.Parallelism;
+
+namespace Hector.Tests.Core.Parallelism
+{
+    public class ParallelismTests
+    {
+        [Fact]
+        public async Task TestParallelLockManager()
+        {
+            Dictionary<string, int> data = new() { { "one", 0 }, { "two", 0 } };
+            using ParallelLockManager<string> mutex = new();
+
+            List<Task> tasks = [];
+            foreach (int i in Enumerable.Range(1, 1000))
+            {
+                bool isEven = i % 2 == 0;
+                string key = isEven ? "one" : "two";
+                Task<int> task = mutex.ExecuteLockedCallAsync(key, () => Task.FromResult(data[key]++));
+                //Task<int> task = Task.Run(() => Task.FromResult(data[key]++));
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);
+
+            data["one"].Should().Be(500);
+            data["two"].Should().Be(500);
+        }
+    }
+}
