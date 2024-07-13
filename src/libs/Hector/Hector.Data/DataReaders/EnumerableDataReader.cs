80ffd0a03b5e81a1dad56b4bd46de606f03e9331
Gabriele Ricci <gabriele.ricci@remira.com>
2024-07-13 17:35:06 +0200

    implemented data readers class

---
+++ src/libs/Hector/Hector.Data/DataReaders/EnumerableDataReader.cs
@@ -0,0 +1,30 @@
+﻿using FastMember;
+using System;
+using System.Collections.Generic;
+
+namespace Hector.Data.DataReaders
+{
+    public class EnumerableDataReader<T> : ObjectDataReader
+    {
+        private readonly TypeAccessor _typeAccessor;
+        private readonly IEnumerator<T> _enumerator;
+
+        private object? _current;
+
+        public EnumerableDataReader(IEnumerable<T> values)
+            : base(typeof(T))
+        {
+            _typeAccessor = TypeAccessor.Create(Type);
+            _enumerator = values.GetEnumerator();
+        }
+
+        public override object GetValue(int i) => _typeAccessor[_current, IndexedMembers[i].Name];
+
+        public override bool Read()
+        {
+            bool returnValue = _enumerator.MoveNext();
+            _current = returnValue ? _enumerator.Current : Type.IsValueType ? Activator.CreateInstance(Type) : null;
+            return returnValue;
+        }
+    }
+}
