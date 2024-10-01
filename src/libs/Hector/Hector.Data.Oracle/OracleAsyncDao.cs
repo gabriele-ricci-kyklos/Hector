b27485191de75b453a3d02f0fbbfd6c437e0688b
Gabriele Ricci <gabriele.ricci@remira.com>
2024-10-01 11:58:18 +0200

    started implementation of bulk insert and implemented sql server version

--- src/libs/Hector/Hector.Data.Oracle/OracleAsyncDao.cs
+++ src/libs/Hector/Hector.Data.Oracle/OracleAsyncDao.cs
@@ -1,5 +1,9 @@
 ﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
 using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Hector.Data.Oracle
 {
@@ -11,5 +15,15 @@ namespace Hector.Data.Oracle
         }
 
         protected override DbConnection GetDbConnection() => new OracleConnection(ConnectionString);
+
+        public override Task ExecuteBulkCopyAsync<T>(IEnumerable<T> items, int batchSize = 0, int timeoutInSeconds = 30, CancellationToken cancellationToken = default)
+        {
+            //Va creato un query builder con una insert into con i nomi campi e parametri dell'entità
+            //Va creata forse una logica common per realizzarlo
+            //Poi settare il paramentro ArrayBindCount dentro OracleCommand con il count delle entità da inserire
+            //Ed eseguire con ExecuteNonQueryAsync
+
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ src/libs/Hector/Hector.Data/Entities/IBaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Hector.Data.Entities
+{
+    public interface IBaseEntity
+    {
+        public string TableName { get; }
+        public string Alias { get; }
+        public bool IsView { get; }
+    }
+}
