2c0ca746082917c031d438a8c25a47430c1bf376
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2024-12-27 13:48:04 +0100

    improved EnumeratorWrapper test

---
+++ src/Hector.Tests.NetFramework/Core/Collections/CollectionTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Hector.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hector.Tests.NetFramework.Core.Collections
+{
+    public class CollectionTests
+    {
+        [Fact]
+        public void TestBictionary()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>()
+            {
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" }
+            };
+
+            Bictionary<int, string> bictionary = new Bictionary<int, string>(dic);
+
+            bictionary.GetForwardValueOrDefault(1).Should().Be("one");
+            bictionary.GetReverseValueOrDefault("one").Should().Be(1);
+        }
+    }
+}
