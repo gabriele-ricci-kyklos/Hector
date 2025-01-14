96b9bebad6e8d58c94226be57261ddeeaeffb3ba
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2025-01-14 20:31:58 +0100

    added ExpandoObjectConverter in json project

---
+++ src/Hector.Json/Converters/ExpandoObjectConverter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Hector.Json.Converters
+{
+    // credits: https://stackoverflow.com/a/65974452/4499267
+    public class ExpandoObjectConverter(FloatFormat floatFormat, UnknownNumberFormat unknownNumberFormat, ObjectFormat objectFormat)
+        : JsonConverter<object>
+    {
+        public FloatFormat FloatFormat { get; } = floatFormat;
+        public UnknownNumberFormat UnknownNumberFormat { get; } = unknownNumberFormat;
+        public ObjectFormat ObjectFormat { get; } = objectFormat;
+
+        public ExpandoObjectConverter() : this(FloatFormat.Double, UnknownNumberFormat.Error, ObjectFormat.Expando) { }
+
+        public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+        {
+            if (value.GetType() == typeof(object))
+            {
+                writer.WriteStartObject();
+                writer.WriteEndObject();
+            }
+            else
+            {
+                JsonSerializer.Serialize(writer, value, value.GetType(), options);
+            }
+        }
+
+        public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+            reader.TokenType switch
+            {
+                JsonTokenType.Null => null!,
+                JsonTokenType.False => false,
+                JsonTokenType.True => true,
+                JsonTokenType.String =>
+                    reader.TryGetDateTime(out DateTime dateValue)
+                    ? dateValue
+                    : reader.TryGetBytesFromBase64(out byte[]? byteArr)
+                        ? byteArr
+                        : reader.TryGetGuid(out Guid guid)
+                            ? guid
+                            : reader.GetString() ?? string.Empty,
+                JsonTokenType.Number => reader switch
+                {
+                    _ when reader.TryGetInt32(out int i) => i,
+                    _ when reader.TryGetInt64(out long l) => l,
+                    _ when FloatFormat == FloatFormat.Decimal && reader.TryGetDecimal(out decimal m) => m,
+                    _ when FloatFormat == FloatFormat.Double && reader.TryGetDouble(out double d) => d,
+                    _ when UnknownNumberFormat == UnknownNumberFormat.JsonElement =>
+                        JsonDocument.ParseValue(ref reader).RootElement.Clone(),
+                    _ => throw new JsonException("Cannot parse current value as a number")
+                },
+                JsonTokenType.StartArray => ReadArray(ref reader, options),
+                JsonTokenType.StartObject => ReadObject(ref reader, options),
+                _ => throw new JsonException($"Unknown token {reader.TokenType}")
+            };
+
+        private object ReadArray(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            ArrayList list = [];
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndArray)
+                {
+                    return list.Count > 0 ? list.ToArray(list[0].GetType()) : Array.Empty<object>();
+                }
+                list.Add(Read(ref reader, typeof(object), options));
+            }
+            throw new JsonException();
+        }
+
+        private IDictionary<string, object> ReadObject(ref Utf8JsonReader reader, JsonSerializerOptions options)
+        {
+            IDictionary<string, object> dict = CreateDictionary();
+            while (reader.Read())
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonTokenType.EndObject:
+                        return dict;
+
+                    case JsonTokenType.PropertyName:
+                        {
+                            string key = reader.GetString()!;
+                            reader.Read();
+                            dict.Add(key, Read(ref reader, typeof(object), options));
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            throw new JsonException();
+        }
+
+        private IDictionary<string, object> CreateDictionary() =>
+            ObjectFormat == ObjectFormat.Expando ? new ExpandoObject() : new Dictionary<string, object>();
+    }
+
+    public enum FloatFormat
+    {
+        Double,
+        Decimal,
+    }
+
+    public enum UnknownNumberFormat
+    {
+        Error,
+        JsonElement,
+    }
+
+    public enum ObjectFormat
+    {
+        Expando,
+        Dictionary,
+    }
+}
---
+++ src/Hector.Tests/Json/JsonTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using System.Dynamic;
+using System.Text.Json;
+
+namespace Hector.Tests.Json
+{
+    public class JsonTests
+    {
+        private static readonly JsonSerializerOptions _options = new() { Converters = { new Hector.Json.Converters.ExpandoObjectConverter() } };
+
+        [Fact]
+        public void TestExpandoConverterWrite()
+        {
+            dynamic obj = new ExpandoObject();
+            obj.Prop1 = 1;
+            obj.Prop2 = "lol";
+
+            string json = JsonSerializer.Serialize<ExpandoObject>(obj, _options);
+            json.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public void TestExpandoConverterRead()
+        {
+            string testJson = @"
+{
+  ""isActive"": true,
+  ""isVerified"": false,
+  ""timestamp"": ""2025-01-14T12:34:56Z"",
+  ""byteArray"": ""SGVsbG8gV29ybGQh"",
+  ""uniqueId"": ""d3b07384-d9a6-4f98-b56b-678ab1f8f7cd"",
+  ""description"": ""This is a sample string"",
+  ""nullableProperty"": null,
+  ""smallNumber"": 12345,
+  ""mediumNumber"": 123456789,
+  ""largeNumber"": 1234567890123456789,
+  ""price"": 12345.67,
+  ""customObject"": {
+    ""property1"": ""Custom String"",
+    ""property2"": 987,
+    ""property3"": true
+  }
+}";
+            dynamic obj = JsonSerializer.Deserialize<dynamic>(testJson, _options) ?? throw new Exception();
+            (obj as object).Should().NotBeNull();
+            (obj.isActive is bool).Should().BeTrue();
+            (obj.isVerified is bool).Should().BeTrue();
+            (obj.timestamp is DateTime).Should().BeTrue();
+            (obj.uniqueId is Guid).Should().BeTrue();
+            (obj.description is string).Should().BeTrue();
+            (obj.nullableProperty is null).Should().BeTrue();
+            (obj.smallNumber is int).Should().BeTrue();
+            (obj.mediumNumber is int).Should().BeTrue();
+            (obj.largeNumber is long).Should().BeTrue();
+            (obj.price is double).Should().BeTrue();
+            (obj.customObject is ExpandoObject).Should().BeTrue();
+        }
+    }
+}
