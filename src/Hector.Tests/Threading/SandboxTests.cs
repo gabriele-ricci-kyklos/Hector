91f7fa17906c0a9b6897311380362bf92fb7b48a
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2025-01-15 10:55:56 +0100

    created Hector.Threading project

---
+++ src/Hector.Tests/Threading/SandboxTests.cs
@@ -0,0 +1,26 @@
+﻿using Hector.Threading.Caching;
+using System.Diagnostics;
+
+namespace Hector.Tests
+{
+    public class ThreadingTests
+    {
+        [Fact]
+        public async Task TestCaching()
+        {
+            using MemCache<int, string> cache = new(100000);
+            Stopwatch sw = Stopwatch.StartNew();
+
+            foreach (int i in Enumerable.Range(0, 100000))
+            {
+                _ = cache.GetOrCreateAsync(i, (c) => { return ValueTask.FromResult("lol"); });
+            }
+
+            sw.Stop();
+            var elapsed = sw.ElapsedMilliseconds;
+
+            await cache.GetOrCreateAsync(2, (c) => ValueTask.FromResult("asd"));
+            await cache.GetOrCreateAsync(2, (c) => ValueTask.FromResult("asd"));
+        }
+    }
+}
