a2fa0a53199a767cde450cb5dd92d5fc87b16419
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2025-01-25 13:47:55 +0100

    removed data tests and added connection string retrieving from file

--- src/Hector.Tests/Data/OracleTests.cs
+++ src/Hector.Tests/Data/OracleTests.cs
@@ -10,64 +10,11 @@ namespace Hector.Tests.Data
     {
         private static IAsyncDao NewAsyncDao()
         {
-            AsyncDaoOptions options = new("Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=172.16.10.53)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SID=rmxora)));User Id=RMX;Password=RMX;", "RMX", false);
+            AsyncDaoOptions options = File.ReadAllText("..\\..\\..\\..\\..\\data\\TestConnectionStrings\\connstring_sqlserver.txt").GetNonNullOrThrow(nameof(connectionString));
             IAsyncDaoHelper daoHelper = new OracleAsyncDaoHelper(options.IgnoreEscape);
             IDbConnectionFactory connectionFactory = new OracleDbConnectionFactory(options.ConnectionString);
             IAsyncDao dao = new OracleAsyncDao(options, daoHelper, connectionFactory);
             return dao;
-        }
-
-        [Fact]
-        public async Task TestOracleBulkInsertRaw()
-        {
-            IAsyncDao dao = NewAsyncDao();
-            using OracleConnection conn = new(dao.ConnectionString);
-            await conn.OpenAsync();
-
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = "INSERT INTO BULK (ID, CODE) VALUES (:id, :code)";
-            cmd.CommandType = CommandType.Text;
-            cmd.ArrayBindCount = 3;
-
-            OracleParameter pId = cmd.CreateParameter();
-            pId.OracleDbType = OracleDbType.Int32;
-            pId.Value = new int[] { 1, 2, 3 };
-            cmd.Parameters.Add(pId);
-
-            OracleParameter pCode = cmd.CreateParameter();
-            pCode.OracleDbType = OracleDbType.Varchar2;
-            pCode.Value = new string[] { "one", "two", "three" };
-            cmd.Parameters.Add(pCode);
-
-            await cmd.ExecuteNonQueryAsync();
-        }
-
-        [Fact]
-        public async Task TestOracleBulkInsert()
-        {
-            IAsyncDao dao = NewAsyncDao();
-
-            BulkItem[] items =
-                Enumerable
-                    .Range(0, 1000)
-                    .Select(x => new BulkItem { Id = x, Code = x.ToString() })
-                    .ToArray();
-
-            await dao.ExecuteBulkCopyAsync(items);
-        }
-
-        [Fact]
-        public async Task TestOracleUpsert()
-        {
-            IAsyncDao dao = NewAsyncDao();
-
-            BulkItem[] items =
-                Enumerable
-                    .Range(0, 1000)
-                    .Select(x => new BulkItem { Id = x, Code = "asd" })
-                    .ToArray();
-
-            await dao.ExecuteUpsertAsync(items);
         }
     }
 }
--- src/Hector.Tests/Data/SqlServerTests.cs
+++ src/Hector.Tests/Data/SqlServerTests.cs
@@ -14,115 +14,12 @@ namespace Hector.Tests.Data
     {
         private static IAsyncDao NewAsyncDao()
         {
-            AsyncDaoOptions options = new("Data Source=kkritstgdb.kyklos.local;Initial Catalog=Remira_Dev_JEK;Persist Security Info=True;User Id=rit-stg-mix;Password=3VCjmnxQxJVnDQxpHg2s;TrustServerCertificate=True", "dbo", false);
+            string connectionString = File.ReadAllText("..\\..\\..\\..\\..\\data\\TestConnectionStrings\\connstring_sqlserver.txt").GetNonNullOrThrow(nameof(connectionString));
+            AsyncDaoOptions options = new(connectionString, "dbo", false);
             IAsyncDaoHelper daoHelper = new SqlServerAsyncDaoHelper(options.IgnoreEscape);
             IDbConnectionFactory connectionFactory = new SqlServerDbConnectionFactory(options.ConnectionString);
             IAsyncDao dao = new SqlServerAsyncDao(options, daoHelper, connectionFactory);
             return dao;
-        }
-
-        [Fact]
-        public async Task TestIAsyncDao()
-        {
-            IAsyncDao dao = NewAsyncDao();
-
-            IQueryBuilder queryBuilder =
-                dao
-                    .NewQueryBuilder()
-                    .SetQuery("select * from JobTimes");
-
-            var results = await dao.ExecuteSelectQueryAsync<Result>(queryBuilder);
-
-            var properties = typeof(Result).GetHierarchicalOrderedPropertyList().Select(x => x.Name).ToArray();
-            var reader = new EnumerableDbDataReader<Result>(results);
-
-            using var conn = new SqlConnection(dao.ConnectionString);
-            await conn.OpenAsync();
-            using (var bcp = new SqlBulkCopy(conn))
-            {
-                bcp.DestinationTableName = "JobTimes2";
-                await bcp.WriteToServerAsync(reader);
-            }
-        }
-
-        [Fact]
-        public async Task TestSqlServerTableType()
-        {
-            IAsyncDao dao = NewAsyncDao();
-
-            const string sql = @"
-DECLARE @tableType T_Divisions
-SELECT * from @tableType
-";
-
-            using SqlConnection conn = new SqlConnection(dao.ConnectionString);
-            await conn.OpenAsync();
-
-            using SqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = sql;
-            cmd.CommandType = CommandType.Text;
-
-            SqlDataReader reader = await cmd.ExecuteReaderAsync(CommandBehavior.KeyInfo);
-            DataTable schemaTable = reader.GetSchemaTable();
-        }
-
-        [Fact]
-        public async Task TestSqlServerUpsert()
-        {
-            IAsyncDao dao = NewAsyncDao();
-
-            BulkItem[] items =
-                Enumerable
-                    .Range(0, 1000)
-                    .Select(x => new BulkItem { Id = x, Code = "asd" })
-                    .ToArray();
-
-            await dao.ExecuteUpsertAsync(items);
-        }
-
-        [Fact]
-        public async Task TestSqlServerBulkInsert()
-        {
-            IAsyncDao dao = NewAsyncDao();
-
-            BulkItem[] items =
-                Enumerable
-                    .Range(0, 1000)
-                    .Select(x => new BulkItem { Id = x, Code = x.ToString() })
-                    .ToArray();
-
-            await dao.ExecuteBulkCopyAsync(items);
-        }
-
-        [Fact]
-        public async Task TestSqlServerScalar()
-        {
-            IAsyncDao dao = NewAsyncDao();
-
-            int c = await dao.ExecuteScalarAsync<int>("select count(*) from [BULK]");
-            c.Should().BePositive();
-        }
-
-        [Fact]
-        public async Task TestSqlServerTransaction()
-        {
-            IAsyncDao dao = NewAsyncDao();
-
-            await dao
-                .ExecuteInTransactionAsync
-                (async tdao =>
-                {
-                    await tdao.ExecuteNonQueryAsync("update [bulk] set code = 'lol' where id = 1").ConfigureAwait(false);
-                    await tdao.ExecuteNonQueryAsync("update [bulk] set code = 'lol' where id = 2").ConfigureAwait(false);
-                });
-        }
-
-        [Fact]
-        public async Task TestTableTypeDefinition()
-        {
-            IAsyncDao dao = NewAsyncDao();
-            TableTypeHelper tableTypeHelper = new(dao);
-            await tableTypeHelper.CreateTableTypesForEntitiesAsync(Assembly.GetExecutingAssembly());
         }
     }
 }
