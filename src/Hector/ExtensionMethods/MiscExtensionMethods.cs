8a55efd956b9ce4e41df5555b0867c13fead328d
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2025-03-11 18:45:24 +0100

    added TryConvertTo extension method

--- src/Hector/ExtensionMethods/MiscExtensionMethods.cs
+++ src/Hector/ExtensionMethods/MiscExtensionMethods.cs
@@ -25,6 +25,48 @@ namespace Hector
             return retValue;
         }
 
+        public static bool TryConvertTo<T>(this object value, out T? convertedValue)
+        {
+            convertedValue = default;
+
+            if (TryConvertTo(value, typeof(T), out object? convertedRawValue))
+            {
+                convertedValue = (T)convertedRawValue!;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool TryConvertTo(this object value, Type typeTo, out object? convertedValue)
+        {
+            convertedValue = default;
+            typeTo = Nullable.GetUnderlyingType(typeTo) ?? typeTo;
+
+            if (value is null || DBNull.Value.Equals(value))
+            {
+                return false;
+            }
+
+            if (value.GetType() == typeTo)
+            {
+                convertedValue = value;
+            }
+            else
+            {
+                try
+                {
+                    convertedValue = Convert.ChangeType(value, typeTo);
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         public static T? ToEnum<T>(this string s, T? failureValue = default)
             where T : struct
         {
