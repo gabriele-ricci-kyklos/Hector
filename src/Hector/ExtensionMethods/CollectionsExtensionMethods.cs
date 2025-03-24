e9b16a617e3d1602588a862b4945d6787bec4d55
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2025-03-24 15:02:56 +0100

    moved misplaced dictionary ext methods

--- src/Hector/ExtensionMethods/CollectionsExtensionMethods.cs
+++ src/Hector/ExtensionMethods/CollectionsExtensionMethods.cs
@@ -124,45 +124,5 @@ namespace Hector
             Array.Copy(data, index, result, 0, length);
             return result;
         }
-
-        internal enum DictMergeStrategy { Override, Keep }
-
-        public static Dictionary<K, V> MergeLeft<K, V>(this Dictionary<K, V> me, params Dictionary<K, V>[] others)
-            where K : notnull =>
-            me.Merge(DictMergeStrategy.Keep, others);
-
-        public static Dictionary<K, V> MergeRight<K, V>(this Dictionary<K, V> me, params Dictionary<K, V>[] others)
-            where K : notnull =>
-            me.Merge(DictMergeStrategy.Override, others);
-
-        private static Dictionary<K, V> Merge<K, V>(this Dictionary<K, V> me, DictMergeStrategy mergeStrategy, params Dictionary<K, V>[] others)
-            where K : notnull
-        {
-            int capacity = me.Count + others.Sum(x => x.Count);
-            Dictionary<K, V> newMap = new(me, me.Comparer);
-
-            var allData =
-                others
-                    .SelectMany(x => x);
-
-            foreach (KeyValuePair<K, V> p in allData)
-            {
-                if (!me.TryGetValue(p.Key, out V? value))
-                {
-                    newMap[p.Key] = p.Value;
-                }
-                else
-                {
-                    newMap[p.Key] = mergeStrategy switch
-                    {
-                        DictMergeStrategy.Override => p.Value,
-                        DictMergeStrategy.Keep => value,
-                        _ => throw new NotSupportedException()
-                    };
-                }
-            }
-
-            return newMap;
-        }
     }
 }
--- src/Hector/ExtensionMethods/DictionaryExtensionMethods.cs
+++ src/Hector/ExtensionMethods/DictionaryExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Hector
@@ -11,5 +12,45 @@ namespace Hector
                 .Where(dict.ContainsKey)
                 .Select(k => dict[k])
                 .ToArray();
+
+        internal enum DictMergeStrategy { Override, Keep }
+
+        public static Dictionary<K, V> MergeLeft<K, V>(this Dictionary<K, V> me, params Dictionary<K, V>[] others)
+            where K : notnull =>
+            me.Merge(DictMergeStrategy.Keep, others);
+
+        public static Dictionary<K, V> MergeRight<K, V>(this Dictionary<K, V> me, params Dictionary<K, V>[] others)
+            where K : notnull =>
+            me.Merge(DictMergeStrategy.Override, others);
+
+        private static Dictionary<K, V> Merge<K, V>(this Dictionary<K, V> me, DictMergeStrategy mergeStrategy, params Dictionary<K, V>[] others)
+            where K : notnull
+        {
+            int capacity = me.Count + others.Sum(x => x.Count);
+            Dictionary<K, V> newMap = new(me, me.Comparer);
+
+            var allData =
+                others
+                    .SelectMany(x => x);
+
+            foreach (KeyValuePair<K, V> p in allData)
+            {
+                if (!me.TryGetValue(p.Key, out V? value))
+                {
+                    newMap[p.Key] = p.Value;
+                }
+                else
+                {
+                    newMap[p.Key] = mergeStrategy switch
+                    {
+                        DictMergeStrategy.Override => p.Value,
+                        DictMergeStrategy.Keep => value,
+                        _ => throw new NotSupportedException()
+                    };
+                }
+            }
+
+            return newMap;
+        }
     }
 }
