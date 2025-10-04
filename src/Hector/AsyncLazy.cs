06db6d4f98f04851f3595f68bb84967b0e6eb038
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2025-10-04 15:14:03 +0200

    added AsyncLazy

---
+++ src/Hector/AsyncLazy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Hector
+{
+    public sealed class AsyncLazy<T>(Func<Task<T>> factory)
+    {
+        private readonly Lazy<Task<T>> _instance = new(() => factory());
+
+        public Task<T> Value => _instance.Value;
+    }
+}
