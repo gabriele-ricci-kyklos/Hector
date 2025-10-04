1c4ebe51815d1d0b329e09436b5a659befa92099
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2025-10-04 15:37:19 +0200

    added tasks ext methods

---
+++ src/Hector/ExtensionMethods/TasksExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hector
+{
+    public static class TasksExtensionMethods
+    {
+        public static Task<T> AsResultTask<T>(this T result) => Task.FromResult(result);
+
+        public static async Task<IEnumerable<T>> WhenAllAsync<T>(this IEnumerable<Task<T>> tasks) =>
+            await Task
+                .WhenAll(tasks.ToEmptyIfNull())
+                .ConfigureAwait(false);
+
+        public static ValueTask<T> AsResultValueTask<T>(this T result) => new(result);
+
+        public static ValueTask<T> AsValueTask<T>(this Task<T> result) => new(result);
+    }
+}
