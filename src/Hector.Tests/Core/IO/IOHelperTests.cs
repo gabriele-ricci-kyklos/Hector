170bf68e8a326615724eb1af7073fe24bba08b4f
gabriele-ricci-kyklos <gabriele.ricci@remira.com>
2025-10-10 17:57:27 +0200

    added IOHelper with a few helper methods

---
+++ src/Hector.Tests/Core/IO/IOHelperTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Hector.IO;
+
+namespace Hector.Tests.Core.IO
+{
+    public class IOHelperTests
+    {
+        [Fact]
+        public void TestGetDirectoryName()
+        {
+            IOHelper.GetDirectoryName("C:\\MyDir").Should().NotBeNullOrWhiteSpace();
+            var act = () => IOHelper.GetDirectoryName("C:\\");
+            act.Should().Throw<DirectoryNotFoundException>();
+        }
+
+        [Fact]
+        public void TestDeleteFolders()
+        {
+            const string baseDirPath = @"C:\Temp\Hector";
+
+            Directory.CreateDirectory(baseDirPath);
+
+            string dir1 = baseDirPath.CombinePaths("Folder1");
+            string dir1a = dir1.CombinePaths("a");
+            string dir1b = dir1.CombinePaths("b");
+            string dir2 = baseDirPath.CombinePaths("Folder2");
+            string dir2a = dir2.CombinePaths("a");
+
+            Directory.CreateDirectory(dir1);
+            Directory.CreateDirectory(dir1a);
+            Directory.CreateDirectory(dir1b);
+
+            IOHelper.DeleteFolders(baseDirPath, ["b"]);
+
+            Directory.Exists(dir1a).Should().BeTrue();
+            Directory.Exists(dir1b).Should().BeFalse();
+
+            Directory.CreateDirectory(dir1);
+            Directory.CreateDirectory(dir1a);
+            Directory.CreateDirectory(dir2);
+            Directory.CreateDirectory(dir2a);
+
+            IOHelper.DeleteFolders(baseDirPath, ["a"], ["Folder1"]);
+
+            Directory.Exists(dir1a).Should().BeTrue();
+            Directory.Exists(dir2a).Should().BeFalse();
+
+            string fileName = baseDirPath.CombinePaths("a.txt");
+            File.WriteAllText(fileName, "a");
+
+            IOHelper.DeleteFolders(baseDirPath, deleteFiles: true);
+
+            File.Exists(fileName).Should().BeFalse();
+            Directory.Exists(dir1).Should().BeFalse();
+
+            Directory.Delete(baseDirPath);
+        }
+    }
+}
---
+++ src/Hector/IO/IOHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Hector.IO
+{
+    public static class IOHelper
+    {
+        public static string GetDirectoryName(string path) => Path.GetDirectoryName(path) ?? throw new DirectoryNotFoundException("No directory retrieved");
+
+        public static void DeleteFolders(string path, string[]? dirsToDelete = null, string[]? pathTokensToExclude = null, bool deleteFiles = false)
+        {
+            foreach (string dir in Directory.EnumerateDirectories(path, "*", SearchOption.AllDirectories))
+            {
+                DirectoryInfo di = new(dir);
+                if (pathTokensToExclude is not null && FindDirectories(di, pathTokensToExclude))
+                {
+                    continue;
+                }
+
+                string dirName = di.Name;
+                if (dirsToDelete is null || dirsToDelete.Contains(dirName, StringComparer.OrdinalIgnoreCase))
+                {
+                    di.Delete(true);
+                }
+            }
+
+            if (!deleteFiles)
+            {
+                return;
+            }
+
+            foreach (string file in Directory.EnumerateFiles(path))
+            {
+                File.Delete(file);
+            }
+        }
+
+        private static bool FindDirectories(DirectoryInfo di, string[] directoryNames)
+        {
+            bool found = false;
+            while (di.Parent is not null)
+            {
+                if (directoryNames.Contains(di.Name, StringComparer.OrdinalIgnoreCase))
+                {
+                    found = true;
+                    break;
+                }
+                di = di.Parent;
+            }
+            return found;
+        }
+    }
+}
